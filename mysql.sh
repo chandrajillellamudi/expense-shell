300e2f9aeedeecf192bd08a208856cc1ed2ddbec
chandrajillellamudi <chandrajillellamudi77@gmail.com>
2025-10-31 07:21:25 +0530

    DB

--- mysql.sh
+++ mysql.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 # Install MySQL
 ID=$(id -u)
-TIME_STAMP=$(date '+%Y-%m-%d %H:%M:%S')
-SCRIPT_NAME=$(echo $0 | cut -d'.' -f1)
-LOGFILE=/tmp/${SCRIPT_NAME}-${TIME_STAMP}.log
+TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 RED="\e[31m"
 GREEN="\e[32m"
 YELLOW="\e[33m"
 NO="\e[0m"
-if [ $ID -ne 0 ]; then
-  echo -e "${RED}Please run as root user${NO}"
-    exit 1
-else
-  echo -e "${GREEN}Running as root user${NO}"
-fi
-validate(){
+
+Validate(){
   if [ $1 -ne 0 ]; then
     echo -e "${RED}$2.. failed${NO}" &>> $LOGFILE
     exit 1
@@ -22,11 +17,22 @@ validate(){
     echo -e "${GREEN}$2.. successful${NO}" &>> $LOGFILE
   fi
 }
-dnf install mysql-server -y &>> $LOGFILE
-validate $? "MySQL installation"
-#systemctl enable mysqld &>> $LOGFILE
-#validate $? "Enable MySQL service"  
-#systemctl start mysqld &>> $LOGFILE
-#validate $? "Start MySQL service"
-#mysql_secure_installation --set-root-pass ExpenseApp@123 &>> $LOGFILE
-#validate $? "Secure MySQL installation"
+
+if [ $ID -ne 0 ]; then
+  echo -e "${RED}Please run as root user${NO}"
+    exit 1
+else
+  echo -e "${GREEN}Running as root user${NO}"
+fi
+
+dnf install mysql-server -y &>>$LOGFILE
+Validate $? "MySQL installation"
+
+systemctl enable mysqld &>> $LOGFILE
+Validate $? "Enable MySQL service"
+
+systemctl start mysqld &>> $LOGFILE
+Validate $? "Start MySQL service"
+
+mysql_secure_installation --set-root-pass ExpenseApp@123 &>> $LOGFILE
+Validate $? "Secure MySQL installation"
