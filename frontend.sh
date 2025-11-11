599e718d1bedbfa8ef0213bc0f5c14909e775f9c
chandrajillellamudi <chandrajillellamudi77@gmail.com>
2025-11-11 10:47:16 +0530

    changes

---
+++ frontend.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$( echo $0 | cut -d'.' -f1)
+LOG_FILE=/tmp/${SCRIPT_NAME}_${TIMESTAMP}.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"  
+N="\e[0m"
+if [ $ID -ne 0 ]; then
+    echo -e "${R}Please run as root user${N}"
+    exit 1
+else
+    echo -e "${G}Running as root user${N}"
+fi
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo -e "${R}$2.. failed${N}"
+        exit 1
+    else
+        echo -e "${G}$2.. successful${N}"
+    fi   
+}
+
+dnf install nginx -y &>> $LOG_FILE
+validate $? "Nginx installation"
+
+systemctl enable nginx &>> $LOG_FILE
+validate $? "Enabling Nginx at boot"
+
+systemctl start nginx &>> $LOG_FILE
+validate $? "Starting Nginx service"
+
+rm -rf /usr/share/nginx/html/* &>> $LOG_FILE
+validate $? "Cleaning Nginx default content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>> $LOG_FILE
+validate $? "Downloading frontend code"
+
+cd /usr/share/nginx/html &>> $LOG_FILE
+validate $? "Changing to Nginx html directory"
+
+unzip /tmp/frontend.zip -d /usr/share/nginx/html/ &>> $LOG_FILE
+validate $? "Unzipping frontend code"
